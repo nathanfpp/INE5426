7ef818b7cf2b4b4a2bfeaab95f552fe5586da461
nathanfpp <nathanfrois@hotmail.com>
2016-10-14 16:00:06 -0300

    mais testes de erros adicionados, entretando estou com problema na contagem de linha de detecção do erro em laços e possivelmente funções.

---
+++ projeto2/error_tests/v0.2/expected/3.ex
@@ -0,0 +1,7 @@
+int var: a = 0
+float var: b = 0.0
+bool var: c = false
+= a c
+= b c
+= a / b c
+
---
+++ projeto2/error_tests/v0.4/expected/2.ex
@@ -0,0 +1,8 @@
+int var: a = 0, b = 1, c, d
+bool var: teste_falso = false
+if: & > a b == d a
+then:
+  if: > teste_falso 0
+  then:
+    = c 10
+
