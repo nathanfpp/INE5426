10086ec2786075ffae18eb0d9d4a64187931c9a6
nathanfpp <nathanfrois@hotmail.com>
2016-11-05 11:08:20 -0200

    Gambiarra provisoria na classe nodo_condicao que faz funcionar a interpretação de loops. Temos alguns erros quanto ao reconhecimento do tipo e o novo escopo que não parece salvar as variaveis feitas no escopo de uma dada iteração interpretada do loop.

--- projeto2/nodo_laco.cpp
+++ projeto2/nodo_laco.cpp
@@ -8,6 +8,8 @@
 
 using namespace AST;
 
+/* MÉTODO DEFAULT
+
 Tipo Laco::analisar(AST::TabelaDeSimbolos *tabelaDeSimbolos, int linha, bool analisador) {
 
   // Analisa-se a inicialização do Laço, caso ela exista
@@ -48,6 +50,63 @@ Tipo Laco::analisar(AST::TabelaDeSimbolos *tabelaDeSimbolos, int linha, bool ana
   // Retorna o tipo do nodo
     return tipo;
 }
+
+*/
+
+
+Tipo Laco::analisar(AST::TabelaDeSimbolos *tabelaDeSimbolos, int linha, bool analisador) {
+
+  // Analisa-se a inicialização do Laço, caso ela exista
+    if(inicializacao != NULL) {
+        inicializacao->analisar(tabelaDeSimbolos, linha, analisador);
+    }
+
+  // O teste de um Laço deve ser Booleano
+    Tipo esperado = teste->analisar(tabelaDeSimbolos, linha, analisador);
+    if(esperado != Tipo::boolean) {
+        imprimirErroDeOperacao(Tipo::teste, Tipo::boolean, esperado, linha);
+    }
+
+  // Analisa-se a iteração do Laço, caso ela exista
+    if(iteracao != NULL) { //NAO RECONHECE tipo = for_laco
+       // Caso o código esteja sendo executado, e a condição do teste for verdadeira, ocorre uma iteração
+         if(analisador) { //SE EU CRIO UM NOVO ESCOPO NÃO FUNCIONA
+             while(teste->boolean) {
+                 iteracao->analisar(tabelaDeSimbolos, linha, true);
+                 teste->analisar(tabelaDeSimbolos, linha, true);
+		 if(laco != NULL)
+	         laco->analisar(tabelaDeSimbolos, linha, analisador);
+             }
+        }
+
+      // Caso contrário, a iteração é analisada apenas uma vez
+        else {
+            iteracao->analisar(tabelaDeSimbolos, linha, false);
+        }
+    }
+    else{
+         if(analisador) {
+	      while(teste->boolean) {
+                 teste->analisar(tabelaDeSimbolos, linha, true);
+		 if(laco != NULL)
+	         laco->analisar(tabelaDeSimbolos, linha, analisador);
+             }
+       }
+       //NESSE ESQUEMA BIZONHO, NÃO TENHO COMO INTERPRETAR O DO_WHILE, APENAS INTERPRETAR
+ }
+
+  // Se o conteúdo do laço não for vazio, também deve ser verificado
+    TabelaDeSimbolos *novoEscopo;
+    if(laco != NULL) {
+        novoEscopo = tabelaDeSimbolos->novoEscopo(tabelaDeSimbolos);
+        laco->analisar(novoEscopo, linha, analisador);
+        novoEscopo->retornarEscopo(linha);
+    }
+
+  // Retorna o tipo do nodo
+    return tipo;
+}
+
 
 
 void Laco::imprimir(int espaco, bool novaLinha) {
